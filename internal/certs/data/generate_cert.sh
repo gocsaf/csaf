d54be467dc065f87f0ead35ebcbc1deed8567e96
Marius Goetze <marius.goetze@greenbone.net>
2025-10-14 16:46:47 +0200

    code review: add license header

--- internal/certs/data/generate_cert.sh
+++ internal/certs/data/generate_cert.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# This file is Free Software under the Apache-2.0 License
+# without warranty, see README.md and LICENSES/Apache-2.0.txt for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# SPDX-FileCopyrightText: 2025 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+# Software-Engineering: 2025 Intevation GmbH <https://intevation.de>
 
 # Script to generate the certificates used for testing.
 
