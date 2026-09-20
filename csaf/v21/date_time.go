64063aa2bb976fbe9c6d155017bb25e183ab8812
Jona Grolms <314212134+jonagrolms@users.noreply.github.com>
2026-09-20 21:30:26 +0200

    Enforce CSAF date-time format

--- csaf/v21/date_time.go
+++ csaf/v21/date_time.go
@@ -5,21 +5,40 @@
 package v21
 
 import (
-	"strings"
+	"encoding/json"
+	"fmt"
+	"regexp"
 	"time"
 )
 
 type DateTime string
+
+var dateTimePattern = regexp.MustCompile(`^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(?:\.[0-9]+)?(?:Z|[+-][0-9]{2}:[0-9]{2})$`)
 
 func NewDateTime(value time.Time) DateTime {
 	return DateTime(value.Format(time.RFC3339Nano))
 }
 
 func (value DateTime) Time() (time.Time, error) {
-	normalized := strings.NewReplacer("t", "T", "z", "Z").Replace(string(value))
-	return time.Parse(time.RFC3339Nano, normalized)
+	if !dateTimePattern.MatchString(string(value)) {
+		return time.Time{}, fmt.Errorf("invalid CSAF date-time %q", value)
+	}
+	return time.Parse(time.RFC3339Nano, string(value))
 }
 
 func (value DateTime) String() string {
 	return string(value)
+}
+
+func (value *DateTime) UnmarshalJSON(data []byte) error {
+	var text string
+	if err := json.Unmarshal(data, &text); err != nil {
+		return err
+	}
+	parsed := DateTime(text)
+	if _, err := parsed.Time(); err != nil {
+		return err
+	}
+	*value = parsed
+	return nil
 }
